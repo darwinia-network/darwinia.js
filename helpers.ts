66b33998e721e55e12cebf2c9adb76f2f34e3294
Aki Wu <aki.wu@itering.com>
2022-12-02 15:10:35 +0800

    add general storage fetch function

--- helpers.ts
+++ helpers.ts
@@ -1,9 +1,12 @@
-import { blake2AsU8a, xxhashAsU8a } from '@polkadot/util-crypto';
-import { u8aConcat, u8aToU8a, u8aToHex } from '@polkadot/util';
-import { Metadata, TypeRegistry } from '@polkadot/types';
-import { HexString } from '@polkadot/util/types';
+import {blake2AsU8a, xxhashAsU8a} from '@polkadot/util-crypto';
+import {u8aConcat, u8aToHex, u8aToU8a} from '@polkadot/util';
+import {Metadata, TypeRegistry, Vec} from '@polkadot/types';
+import {HexString} from '@polkadot/util/types';
 
-import { ethers, providers } from "ethers";
+import {ethers, providers} from "ethers";
+import {StorageEntryMetadataV14, StorageHasherV14} from "@polkadot/types/interfaces/metadata/types";
+import {SiLookupTypeId} from "@polkadot/types/interfaces";
+
 type BaseProvider = providers.BaseProvider;
 
 export async function getStorageRaw(provider: BaseProvider, storageKey: Uint8Array): Promise<Uint8Array> {
@@ -48,4 +51,82 @@ export async function getStorage(provider: BaseProvider, registry: TypeRegistry,
     } else {
         return registry.createType(valueMeta.valueType, data).toString();
     }
+}
+
+export function getStorageEntry(metadata: Metadata, prefix: string, method: string): StorageEntryMetadataV14 | null {
+    for (const pallet of metadata.asV14.pallets) {
+        if (pallet.name.toString() == prefix) {
+            const storage = pallet.storage.unwrap();
+            for (const item of storage.items) {
+                if (item.name.toString() == method) {
+                    return item;
+                }
+            }
+        }
+    }
+    return null;
+}
+
+function buildFullStorageKey(input: Array<unknown>, hashers: Vec<StorageHasherV14>, keyTypeId: SiLookupTypeId, metadata: Metadata, partialStorageKey: Uint8Array) {
+    let storageKey = partialStorageKey;
+
+    let keys = hashers.length === 1
+        ? [keyTypeId]
+        : metadata.registry.lookup.getSiType(keyTypeId).def.asTuple;
+
+    for (let i = 0; i < keys.length; i++) {
+        const theKeyObj = input[i];
+        const theHasher = hashers[i];
+        const theKeyTypeId = keys[i];
+
+        // get the scale encoded key data by encoding the input
+        const theKeyType = metadata.registry.createLookupType(theKeyTypeId);
+        const theKeyDataEncoded = metadata.registry.createType(theKeyType, theKeyObj).toU8a();
+
+        // apply hasher
+        if (theHasher.toString() == "Blake2_128Concat") {
+            const theKeyDataAppliedHasher = blake2_128Concat(theKeyDataEncoded);
+            storageKey = u8aConcat(storageKey, theKeyDataAppliedHasher);
+        } else {
+            throw `The hasher ${theHasher.toString()} is not support. Contact Aki for help`;
+        }
+    }
+    return storageKey;
+}
+
+export async function getStorage1(provider: BaseProvider, metadata: Metadata, prefix: string, method: string, input?: Array<unknown>): Promise<string | null> {
+    // 0. FIND STORAGE ENTRY FROM METADATA
+    const storageEntry = getStorageEntry(metadata, prefix, method);
+    if (!storageEntry) {
+        throw "Can not find the storage entry from metadata";
+    }
+
+    // 1. GET STORAGE KEY & THE RESULT TYPE
+    let storageKey = u8aConcat(
+        xxhashAsU8a(prefix, 128), xxhashAsU8a(method, 128)
+    );
+    let valueType;
+
+    if (storageEntry.type.isPlain) {
+        valueType = metadata.registry.createLookupType(storageEntry.type.asPlain);
+    } else if (storageEntry.type.isMap) {
+        const {hashers, key, value} = storageEntry.type.asMap;
+
+        if (!input || input.length != hashers.length) {
+            throw "The `input` param is not correct";
+        }
+
+        storageKey = buildFullStorageKey(input, hashers, key, metadata, storageKey);
+        valueType = metadata.registry.createLookupType(value);
+    } else {
+        throw "Only support plain and map type";
+    }
+    console.debug(`storage key: ${u8aToHex(storageKey)}`);
+
+    // 2. GET RAW STORAGE DATA BY STORAGE KEY
+    let raw = await getStorageRaw(provider, storageKey);
+    console.debug(`        raw: ${raw}`);
+
+    // 3. DECODE THE RAW STORAGE DATA BY THE RESULT TYPE
+    return metadata.registry.createType(valueType, raw).toString();
 }
