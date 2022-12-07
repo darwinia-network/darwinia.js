b1a19eee143f60c975446befdd1631304faee888
Aki Wu <aki.wu@itering.com>
2022-12-07 14:21:27 +0800

    remove old files

--- pallets/indices.ts
+++
@@ -1,7 +0,0 @@
-import {GetStorage} from "../storage";
-
-export const indices = {
-    accounts: async (getStorage: GetStorage, param0: unknown): Promise<string | null> => {
-        return await getStorage('Indices', 'Accounts', param0);
-    },
-};
