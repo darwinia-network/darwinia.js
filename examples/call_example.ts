4caab3218697c78214e20c2781b1ea05d2c3e65a
Aki Wu <aki.wu@itering.com>
2023-01-05 15:27:48 +0800

    update readme

--- examples/call_example.ts
+++ examples/call_example.ts
@@ -6,24 +6,32 @@ async function main(): Promise<void> {
     const provider = new ethers.providers.JsonRpcProvider("https://cors.kahub.in/http://g1.dev.darwinia.network:10000");
     const metadata = buildMetadata(staticMetadata.pangolin2MetaStatic);
 
-    const dispatchPangolin2Call = dispatch(provider, metadata);
-    // const wallet = provider.getSigner();
-    const wallet = new ethers.Wallet("d5dd1909b74029eb3164b10ce84abaf9b0ea379b3ea0d4e2a96241806b8f8175");
+    // const signer = provider.getSigner();
+    const signer = new ethers.Wallet("d5dd1909b74029eb3164b10ce84abaf9b0ea379b3ea0d4e2a96241806b8f8175");
 
     // Way 1
-    const keys = {
-        aura: "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d",
-    };
-    await dispatchPangolin2Call(wallet, "Session", "setKeys", false, keys, "0x");
-
-    // Way 2
     const pangolin2 = clientBuilder.buildPangolin2Client(provider, metadata);
-    await pangolin2.calls.session.setKeys(wallet, keys, "0x")
-
-    // Way 3
+    await pangolin2.calls.session.setKeys(
+      signer,
+      { aura: "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d" }, // keys
+      "0x" // proof
+    )
+  
+    // Way 2
+    const dispatchPangolin2Call = dispatch(provider, metadata);
+    await dispatchPangolin2Call(
+      signer, 
+      "Session", 
+      "setKeys", 
+      false, 
+      { aura: "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d" }, // keys
+      "0x" // proof
+    );
+  
+    // Way 3: this is a customized method added for client convenience
     await setSessionKeys(
         dispatchPangolin2Call,
-        wallet,
+        signer,
         "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"
     );
 }
