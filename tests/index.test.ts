4bdcfba1a984c388792b9cc51220e1a27a35469f
Aki Wu <wuminzhe@gmail.com>
2023-02-27 16:13:57 +0800

    fix tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,51 +1,58 @@
-import { buildMetadata, decodeCall, getCallMeta, getEventMeta } from "../index";
+import { buildMetadata, decodeCall, getCallMeta, getEventMeta, EventData, EventMeta } from "../index";
 import { staticMetadataForTest } from "./metadata_for_test";
 
 describe('metadata', () => {
-    test('call can be decoded from scale codec bytes', () => {
-        const metadata = buildMetadata(staticMetadataForTest);
-        const result =
-            decodeCall(metadata, 'Session', 'setKeys', "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d00");
+  test('call can be decoded from scale codec bytes', () => {
+    const metadata = buildMetadata(staticMetadataForTest);
+    const result =
+      decodeCall(metadata, 'Session', 'setKeys', "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d00");
 
-        expect(result).toStrictEqual(
-            {
-                callIndex: [9, 0],
-                args: {
-                    keys: {
-                        aura: '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'
-                    },
-                    proof: '0x'
-                }
-            }
-        );
+    expect(result).toStrictEqual(
+      {
+        callIndex: [9, 0],
+        args: {
+          keys: {
+            aura: '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'
+          },
+          proof: '0x'
+        }
+      }
+    );
 
-    });
+  });
 
-    test('call meta can be got from metadata', () => {
-        const metadata = buildMetadata(staticMetadataForTest);
-        const result = getCallMeta(metadata, 'System', 'remark_with_event');
+  test('call meta can be got from metadata', () => {
+    const metadata = buildMetadata(staticMetadataForTest);
+    const result = getCallMeta(metadata, 'System', 'remark_with_event');
 
-        expect(result).toStrictEqual(
-            {
-                callIndex: [0, 8],
-                args: ['Lookup12'],
-                belongsTo: 'Lookup144'
-            }
-        );
+    expect(result).toStrictEqual(
+      {
+        callIndex: [0, 8],
+        args: ['Lookup12'],
+        belongsTo: 'Lookup144'
+      }
+    );
 
-    });
+  });
 
-    test('event meta can be got from metadata', () => {
-        const metadata = buildMetadata(staticMetadataForTest);
-        const result = getEventMeta(metadata, 'System', 'ExtrinsicSuccess');
+  test('event meta can be got from metadata', () => {
+    const metadata = buildMetadata(staticMetadataForTest);
+    const result = getEventMeta(metadata, 'System', 'ExtrinsicSuccess');
 
-        expect(result).toStrictEqual(
-            {
-                eventIndex: [0, 0],
-                fields: ['Lookup21'],
-                belongsTo: 'Lookup20'
-            }
-        );
+    console.log(result);
+    expect(result).toStrictEqual({
+      palletName: 'System',
+      eventName: 'ExtrinsicSuccess',
+      eventIndex: [0, 0],
+      fields: [
+        {
+          name: 'dispatch_info',
+          typeName: 'DispatchInfo',
+          lookupType: 'Lookup21'
+        }
+      ],
+      belongsTo: 'Lookup20'
+    } as EventMeta);
 
-    });
+  });
 });
